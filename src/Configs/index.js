6e3d33c4f4314d18b5949e739f73fe9881cf0ebb
Arifzyn <arifzynapis19@gmail.com>
2025-01-17 02:41:04 +0700

    Update index.js

--- src/Configs/index.js
+++ src/Configs/index.js
@@ -1,12 +1,12 @@
 /**
- * @author FrierenDv
+ * @author AR
  * @version 1.0.0
  *
  * Don't forget to star the repo :)
  * @link (https://github.com/Arifzyn19/AR-WRAPPER)
  */
 
-export const BASE_URL = "https://api.arifzyn.tech";
+export const BASE_URL = "https://api.arifzyn.site";
 
 export const Config = {
   headers: {
